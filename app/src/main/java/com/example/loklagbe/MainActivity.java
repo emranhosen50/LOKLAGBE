492779824f90e3f03bfd25263662dbdf656bc130
Emran Hosen Emon <emranhosen.emon11@gmail.com>
2022-03-25 10:14:45 +0600

    Now Perfect this Projects

---
+++ app/src/main/java/com/example/loklagbe/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.loklagbe;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
+    }
+}
